5f381da906b65952390dce734b70d19b34b86029
wmxl <294036748@qq.com>
2020-10-28 04:15:53 +0800

    [yiming] add post controller for student

--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/controller/StudentController.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/controller/StudentController.java
@@ -1,11 +1,10 @@
 package com.thoughtworks.capability.gtb.entrancequiz.controller;
 
+import com.thoughtworks.capability.gtb.entrancequiz.db.DataProvider;
 import com.thoughtworks.capability.gtb.entrancequiz.model.Student;
 import com.thoughtworks.capability.gtb.entrancequiz.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
@@ -20,5 +19,12 @@ public class StudentController {
     public List<Student> findAll(){
         List<Student> list = studentService.findAll();
         return list;
+    }
+    @CrossOrigin()
+    @PostMapping(value = "/add")
+    public void addStudent(@RequestBody Student stu) {
+        System.out.println(stu.getName());
+        int id = DataProvider.students.size();
+        DataProvider.students.add(new Student(id, stu.getName()));
     }
 }
